d7b41c6d09e9f688eae9121b35333c69296b6a42
unknown <xenamancer@gmail.com>
2021-07-19 12:55:38 +0300

    4

--- script.js
+++ script.js
@@ -4,17 +4,29 @@ let listElems = carousel.querySelectorAll('li');
 let position = 0;
 let width = 800;
 let count = 1;
-
+let index = 0;
 carousel.querySelector('.prev').onclick = function () {
+  index = index - count;
   position += width * count;
   //если осталось картинок меньше count для сдвига влево
   position = Math.min(position, 0);
   list.style.marginLeft = position + 'px';
+  if (index < 0) {
+    position = -width * (listElems.length - count);
+    list.style.marginLeft = position + 'px';
+    index = listElems.length - 1;
+  }
 };
 
 carousel.querySelector('.next').onclick = function () {
+  index = index + count;
   position -= width * count;
   //если осталось картинок меньше count для сдвига вправо
   position = Math.max(position, -width * (listElems.length - count));
   list.style.marginLeft = position + 'px';
+  if (index > listElems.length - 1) {
+    position = 0;
+    list.style.marginLeft = position + 'px';
+    index = 0;
+  }
 };
